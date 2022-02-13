ceeca477517608eba09657f1e81d47c7cc5307e1
alexcrrera <99589754+alexcrrera@users.noreply.github.com>
2022-02-13 11:15:27 +0100

    Add files via upload

---
+++ TVC Servo Misalignment.ino
@@ -0,0 +1,28 @@
+
+#include <Servo.h>
+
+Servo myservo;  
+int angle = 90;  // default center position
+char input;
+
+void setup() {
+    myservo.attach(10);  // select your servo's input
+    Serial.begin(9600);
+}
+
+void loop() {
+
+  if(Serial.available()){
+        input = Serial.read();
+        
+        if(input=='2'){
+          angle = angle+1;    
+        }
+        if(input=='1'){
+          angle = angle-1;
+        }
+        Serial.println(angle);
+    }
+    delay(50);
+    myservo.write(angle);
+}
